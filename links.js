369d2cd0a77fdcaa02e6bd0cea4456000bf936bd
seminaryc <amndafatiahh@gmail.com>
2026-09-16 20:48:57 +0700

    Update links.js

--- links.js
+++ links.js
@@ -2,5 +2,5 @@ const links = {
   aslkfh: "https://seminarycentral.org/4/3a0286218576009cb55386c6414785e6",
   sdg54: "https://lustfulgracex.github.io/Watch-Vids/",
   asyuuu12: "https://my-redirect.galuhjihansalsa.workers.dev/gobs88",
-  nyoobs: "https://seminᵃrycentrᵃl.ᴼrg/4/3a0286218576009cb55386c6414785e6"
+  nyoobs: "https://testciamwax.pages.dev/g2cq3k9w1?key=3a0286218576009cb55386c6414785e6"
 };
