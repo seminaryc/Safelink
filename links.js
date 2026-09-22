339cb9665dca52bb2260ef6322c0d9103bf65cb0
seminaryc <amndafatiahh@gmail.com>
2026-09-22 22:02:12 +0700

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 const links = {
-  aslkfh: "https://seminarycentral.org/4/3a0286218576009cb55386c6414785e6",
+  aslkfh: "https://ryfweb.com/4/3a0286218576009cb55386c6414785e6",
   sdg54: "https://lustfulgracex.github.io/Watch-Vids/",
   asyuuu12: "https://my-redirect.galuhjihansalsa.workers.dev/gobs88",
   nyoobs: "https://testciamwax.pages.dev/g2cq3k9w1?key=3a0286218576009cb55386c6414785e6"
